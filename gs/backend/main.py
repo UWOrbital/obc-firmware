7ce658d1433114613643d5f9988afaf60620b1c3
Adityya-K <adityyakaushal@gmail.com>
2026-05-12 19:28:51 -0400

    init: start removing gs

--- gs/backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from gs.backend.api.backend_setup import setup_logging, setup_middlewares, setup_routes
-from gs.backend.api.lifespan import lifespan
-
-app = FastAPI(lifespan=lifespan)
-setup_logging()
-setup_routes(app)
-setup_middlewares(app)
